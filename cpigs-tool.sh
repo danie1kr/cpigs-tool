e321644c9c6d23bff04dd2f7da7ab25292fb9372
danie1kr <37019555+danie1kr@users.noreply.github.com>
2024-02-28 21:10:19 +0100

    moonlight 1st iteration

--- cpigs-tool.sh
+++ cpigs-tool.sh
@@ -3,18 +3,47 @@
 SELF=$0
 MODULES=()
 
-function register()
+CPIGS_WORKSPACE=../cpigs-tool-workspace
+mkdir -p $CPIGS_WORKSPACE
+
+function cpigs_register()
 {
    local module=$1
    MODULES+=($module)
 }
 
+function cpigs_error()
+{
+    echo $1
+    exit -1
+}
+
+function cpigs_ask()
+{
+    read -p "$1 [$2]: " answer
+    answer=${answer:-$2}
+    echo $answer
+}
+
+function cpigs_link()
+{
+    local pos=$1
+    local title=$2
+    local script=$3
+    local icon=$4
+    echo "linking $title on pos $pos with script $script and icon $icon"
+}
 
 MODULES_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
 # todo: source all
 source "$MODULES_DIR/modules/moonlight.sh"
+
+function bootstrap()
+{
+    sudo apt install -y librsvg2-bin
+}
 
 function help()
 {
--- modules/moonlight.sh
+++ modules/moonlight.sh
@@ -1,4 +1,51 @@
 #!/bin/bash
+
+CPIGS_WORKSPACE_MOONLIGHT=$CPIGS_WORKSPACE/moonlight
+mkdir -p $CPIGS_WORKSPACE_MOONLIGHT
+
+function moonlight_install()
+{
+    pushd .
+    cd $CPIGS_WORKSPACE_MOONLIGHT
+
+    curl -1sLf 'https://dl.cloudsmith.io/public/moonlight-game-streaming/moonlight-qt/setup.deb.sh' | sudo -E bash
+    sudo apt install -y moonlight-qt 
+
+    wget 'https://raw.githubusercontent.com/moonlight-stream/moonlight-qt/master/app/res/moonlight.svg'
+
+    rsvg-convert -w 256 -h 256 moonlight.svg -o moonlight.png
+
+    touch installed
+
+    popd
+}
+
+function moonlight_update()
+{
+    if ! [ -f $CPIGS_WORKSPACE_MOONLIGHT/installed ]; then
+        cpigs_error "Please install first"
+        return -1
+    fi
+    
+    sudo apt update
+    sudo apt install --only-upgrade -y moonlight-qt
+}
+
+function moonlight_link()
+{
+    if ! [ -f $CPIGS_WORKSPACE_MOONLIGHT/installed ]; then
+        cpigs_error "Please install first"
+        return -1
+    fi
+
+    local pos=`cpigs_ask "Launcher Item Position" "20"`
+    local title=`cpigs_ask "Launcher Item Title" "moonlight"`
+
+    local script="/tmp/cpigs/script"
+    local icon="$CPIGS_WORKSPACE_MOONLIGHT/moonlight.png"
+    cpigs_link $pos $title $script $icon
+}
+
 function moonlight()
 {
 while [ "$#" -gt 0 ]; do
@@ -7,10 +54,22 @@ while [ "$#" -gt 0 ]; do
          echo "remote game streaming, see https://moonlight-stream.org/"
          shift 1
         ;;
-      help)
-        echo "install"
-        echo "update"
-        echo "shortcut"
+      install)
+          moonlight_install
+          return 0
+          ;;
+      update)
+          moonlight_update
+          return 0
+          ;;
+      link)
+          moonlight_link
+          return 0
+          ;;
+      --help)
+        echo "install: add moonlight-qt repository, download and install it"
+        echo "update: apt update moonlight-qt"
+        echo "link: create a link in the launcher for a machine and application"
         shift 1
         ;;
       *)
@@ -21,4 +80,4 @@ while [ "$#" -gt 0 ]; do
   done
 }
 
-register "moonlight"
+cpigs_register "moonlight"
